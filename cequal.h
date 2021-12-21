4b72b285951b64992507bbc923edc8307fe1f7b0
superpi15 <nhs@WSL.localdomain>
2021-12-21 19:34:06 +0800

    use only fft

--- cequal.h
+++ cequal.h
@@ -134,10 +134,6 @@ inline void Ceq_Man_t::run_prefetch_filter(std::ostream * postr, int fPlot, cons
 
 	int half_window_size = this->window_size/2;
 
-	std::vector< std::vector<double> > vFMatrixSin, vFMatrixCos, vFMatrixSinInv, vFMatrixCosInv;
-	std::vector<double> vBandFreq;
-	std::vector<double> vImg, vReal;
-
 	int max_freq = header.sample_rate / 2; // The smallest wave crest and through are matched a input sample
 	int size_of_window = 512;
 	double min_freq = (double) max_freq / (size_of_window / 2);
@@ -154,32 +150,14 @@ inline void Ceq_Man_t::run_prefetch_filter(std::ostream * postr, int fPlot, cons
 		vData   [i].resize( num_samples + size_of_window, 0.0f );
 		vDataOut[i].resize( num_samples, 0.0f );
 	}
-	vFMatrixSin.resize(size_of_window);
-	vFMatrixCos.resize(size_of_window);
-	vFMatrixSinInv.resize(size_of_window);
-	vFMatrixCosInv.resize(size_of_window);
-	vBandFreq  .resize(size_of_window);
-	vImg       .resize(size_of_window);
-	vReal      .resize(size_of_window);
-	double freq_step = (max_freq - min_freq)/size_of_window;
-	for(int i = 0; i < size_of_window; i ++){ // band
-		vBandFreq[i] = (double) min_freq + freq_step * i;
-		vFMatrixSin[i].resize(size_of_window);
-		vFMatrixCos[i].resize(size_of_window);
-		vFMatrixSinInv[i].resize(size_of_window);
-		vFMatrixCosInv[i].resize(size_of_window);
-		//printf("%9.2f ", vBandFreq[i]);
-		for(int j = 0; j < size_of_window; j ++){ // time 
-			vFMatrixSin[i][j] = sin( angular_unit * (j+1) * (i+1) );
-			vFMatrixCos[i][j] = cos( angular_unit * (j+1) * (i+1) );
-			vFMatrixSinInv[i][j] = sin( - angular_unit * (j+1) * (i+1) );
-			vFMatrixCosInv[i][j] = cos( - angular_unit * (j+1) * (i+1) );
-			//if(postr && fPlot && 0==i ) (*postr) << vFMatrixSin[i][j]<<"\n";
-		}
-		//if(postr && fPlot && 5==i ) (*postr) << "\n";
-	}
-	//printf("\n");
 
+	std::vector<double> vInvSin, vInvCos;
+	vInvSin.resize(size_of_window);
+	vInvCos.resize(size_of_window);
+	for(int i = 0; i < size_of_window; i ++){
+		vInvSin[i] = sin( - angular_unit * i * half_window_size );
+		vInvCos[i] = cos( - angular_unit * i * half_window_size );
+	}
 
 
 	for(int i = 0; i < num_samples; i ++){
@@ -200,42 +178,18 @@ inline void Ceq_Man_t::run_prefetch_filter(std::ostream * postr, int fPlot, cons
 
 	for(int i = 0; i < num_samples; i ++){
 		for(int j = 0; j < header.channels; j ++){
-
-			if(fft){
-				std::vector<double> vDataTmp;
-				vDataTmp.resize(size_of_window);
-				for(int l = 0; l < size_of_window; l ++){
-					vDataTmp[l] = vData[j][l+i];
-				}
-				std::vector<Cpx_t> vCpx;
-				run_fft(vDataTmp, &vCpx);
-
-				double real = 0;
-				for(int k = 0; k < size_of_window; k ++){ // k-th freq 
-					real += vCpx[k].real * vFMatrixCosInv[k][half_window_size]
-					 	- vCpx[k].img * vFMatrixSinInv[k][half_window_size];
-				}
-				vDataOut[j][i] = real/size_of_window;
-			} else {
-				#pragma omp parallel for num_threads(8)
-				for(int k = 0; k < size_of_window; k ++){ // k-th freq 
-					//double img = 0;
-					double real = 0;
-					for(int l = 0; l < size_of_window; l ++){
-						//img  += (double) vData[j][l+i] * vFMatrixSin[k][l];
-						real += (double) vData[j][l+i] * vFMatrixCos[k][l];
-					}
-					//vImg [k] = img;
-					vReal[k] = real;
-				}
-
-				double real = 0;
-				for(int k = 0; k < size_of_window; k ++){ // k-th freq 
-					real += vReal[k] * vFMatrixCosInv[k][half_window_size];// - vImg[k] * vFMatrixSinInv[k][l];
-				}
-				vDataOut[j][i] = real/size_of_window;
+			std::vector<double> vDataTmp;
+			vDataTmp.resize(size_of_window);
+			for(int l = 0; l < size_of_window; l ++){
+				vDataTmp[l] = vData[j][l+i];
 			}
+			std::vector<Cpx_t> vCpx;
+			run_fft(vDataTmp, &vCpx);
 
+			double real = 0;
+			for(int k = 0; k < size_of_window; k ++) // k-th freq 
+				real += vCpx[k].real * vInvCos[k] - vCpx[k].img * vInvSin[k];
+			vDataOut[j][i] = real/size_of_window;
 		}
 	}
 
