8c19570ab260c2b7020b8f2de531f6bffaf51aef
superpi15 <nhs@WSL.localdomain>
2021-12-28 23:43:29 +0800

    rearrange butterfly structure

--- cequal.h
+++ cequal.h
@@ -256,23 +256,33 @@ inline void Ceq_Man_t::setDefault(){
 	vBandLabel[nBand-1] = 'x';
 }
 
+inline uint32_t uint32_bit_reverse(uint32_t x, uint32_t width)
+{
+    // https://stackoverflow.com/questions/9144800/c-reverse-bits-in-unsigned-integer
+    x = ((x >> 1) & 0x55555555u) | ((x & 0x55555555u) << 1);
+    x = ((x >> 2) & 0x33333333u) | ((x & 0x33333333u) << 2);
+    x = ((x >> 4) & 0x0f0f0f0fu) | ((x & 0x0f0f0f0fu) << 4);
+    x = ((x >> 8) & 0x00ff00ffu) | ((x & 0x00ff00ffu) << 8);
+    x = ((x >> 16) & 0xffffu) | ((x & 0xffffu) << 16);
+    return x>>=(32-width);
+}
+
 inline void Ceq_Man_t::run_fft(std::vector<Cpx_t>& vCpxIn, std::vector<Cpx_t>& vResult, const std::vector<Cpx_t>& vDataIn, std::vector<Cpx_t> * pvCpx, bool fifft){
 	//std::vector<Cpx_t> vCpxIn, vResult;
 	int size_of_window = vDataIn.size();
 	
-//	for(int i = size_of_window; i > 0 ; i >>=1 )
-//		assert( !(i&1) || 1==i );
-
+	uint32_t pow2 = 0;	
+	for(int i = size_of_window; i > 0 ; i >>=1, pow2 ++ )
+		assert( !(i&1) || 1==i );
+	pow2 -= 1;
 	int half_window_size = size_of_window/2;
 	if( vCpxIn.size() < size_of_window )
 		vCpxIn .resize(size_of_window);
 	if( vResult.size() < size_of_window )
 		vResult.resize(size_of_window);
-	for(int i = 0; i < half_window_size; i +=2){
-		vCpxIn[ i ]    = vDataIn[ i ];
-		vCpxIn[ i + 1] = vDataIn[ i + half_window_size ];
-		vCpxIn[ i + half_window_size ]     = vDataIn[i + 1 ];
-		vCpxIn[ i + half_window_size + 1 ] = vDataIn[i + 1 + half_window_size ];
+	for(int i = 0; i < size_of_window; i ++){
+		int idx = uint32_bit_reverse(i,pow2);
+		vCpxIn[ i ]    = vDataIn[ idx ];
 	}
 
 
